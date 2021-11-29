04c75f78b95cc0b0fc9b220f9c5e6e021814d551
Servicios Corporativos en Telecomunicaciones <noc@wificolombia.net>
2021-11-29 08:54:10 -0500

    Migrado a Cloud MongoDB

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(express.static('public'))
 const routes = require('./routes/routes')
 app.use(routes)
 
-app.listen(3000,()=>{
-  console.log('Server on http://localhost:3000')
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+  console.log('Server on http://localhost:'+PORT)
 })
