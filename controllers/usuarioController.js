e67f2244a85ff9db07b3b6d2d8a25c1fe6ae06b8
Servicios Corporativos en Telecomunicaciones <noc@wificolombia.net>
2021-12-01 12:57:51 -0500

    UsuarioController add Login

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,8 @@
 const Usuario = require('../models/Usuario')
+const Cliente = require('../models/Cliente')
+const Proveedor = require('../models/Proveedor')
+const Producto = require('../models/Producto')
+
 const bcrypt = require('bcrypt')
 const BCRYPT_SALT_ROUNDS = 12
 
@@ -18,7 +22,16 @@ async function loginProcess (req,res,next){
     console.error('Usuario no econtrado')
     return res.status(500).render('login', {'message': 'Usuario no encontrado'})
   }else{
-    return res.status(200).render('index')
+    const usuarios = await Usuario.countDocuments({})
+    const clientes = await Cliente.countDocuments({})
+    const proveedores = await Proveedor.countDocuments({})
+    const productos = await Producto.countDocuments({})
+    return res.status(200).render('index', {
+      usuarios: usuarios,
+      clientes: clientes,
+      proveedores: proveedores,
+      productos: productos,
+    })
   }
 }
 
